0bbbf6552636a6fdc02bb97af7fcdd5f6bb0e900
kaiserman <rubskaiserman@gmail.com>
2023-07-01 17:27:24 -0300

    fix: structuring data as multiple arrays instead of one

--- src/main.c
+++ src/main.c
@@ -4,27 +4,42 @@
 
 struct IrisInformation
 {
-    float sepal_length;
-    float sepal_width;
-    float petal_length;
-    float petal_width;
+    double sepal_length;
+    double sepal_width;
+    double petal_length;
+    double petal_width;
     char *class;
 };
-const int DATA_SIZE = 151;
 
-struct IrisInformation *getDataFrom(const char *path)
+struct IrisInformationArrays
+{
+    char *atributos[5];
+    double caracteristicas[150][4];
+    char *class[150];
+};
+
+const int DATA_SIZE = 150;
+
+struct IrisInformationArrays *getDataFrom(const char *path)
 {
     struct IrisInformation *data = malloc(DATA_SIZE * sizeof(struct IrisInformation));
+    if (data == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
 
     FILE *file = fopen(path, "r");
     if (file == NULL)
     {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Error opening the file.\n");
         exit(1);
     }
+
     char *line = malloc(100 * sizeof(char));
+    fgets(line, 100, file); // skipping column name line
+
     int i = 0;
-    fgets(line, sizeof(line), file); // dropping column name line
     while (fgets(line, 100, file) != NULL)
     {
         struct IrisInformation iris;
@@ -45,36 +60,59 @@ struct IrisInformation *getDataFrom(const char *path)
     }
 
     fclose(file);
-    return data;
+    free(line);
+
+    struct IrisInformationArrays *arrays = malloc(sizeof(struct IrisInformationArrays));
+    if (arrays == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
+        arrays->caracteristicas[i][0] = data[i].sepal_length;
+        arrays->caracteristicas[i][1] = data[i].sepal_width;
+        arrays->caracteristicas[i][2] = data[i].petal_length;
+        arrays->caracteristicas[i][3] = data[i].petal_width;
+        arrays->class[i] = data[i].class;
+    }
+    
+
+    arrays->atributos[0] = "sepal_length";
+    arrays->atributos[1] = "sepal_width";
+    arrays->atributos[2] = "petal_length";
+    arrays->atributos[3] = "petal_width";
+    arrays->atributos[4] = "class";
+
+    return arrays;
 }
 
 int main()
 {
     const char *path = "../database/iris.csv";
-    struct IrisInformation *data = getDataFrom(path);
-    if (data != NULL)
+    struct IrisInformationArrays *dataArrays = getDataFrom(path);
+    if (dataArrays != NULL)
     {
         // Printing the data for verification
         for (int j = 0; j < DATA_SIZE; j++)
         {
             printf("Iris %d:\n", j + 1);
-            printf("Sepal Length: %.2f\n", data[j].sepal_length);
-            printf("Sepal Width: %.2f\n", data[j].sepal_width);
-            printf("Petal Length: %.2f\n", data[j].petal_length);
-            printf("Petal Width: %.2f\n", data[j].petal_width);
-            printf("Class: %s\n", data[j].class);
+            printf("Sepal Length: %.2f\n", dataArrays->caracteristicas[j][0]);
+            printf("Sepal Width: %.2f\n", dataArrays->caracteristicas[j][1]);
+            printf("Petal Length: %.2f\n", dataArrays->caracteristicas[j][2]);
+            printf("Petal Width: %.2f\n", dataArrays->caracteristicas[j][3]);
+            printf("Class: %s\n", dataArrays->class[j]);
             printf("---------------------\n");
         }
-
-
-
-
-        for (int j = 0; j < DATA_SIZE; j++)
-        {
-            free(data[j].class);
-        }
-        free(data);
     }
+
+    // Freeing allocated memory
+    for (int j = 0; j < DATA_SIZE; j++)
+    {
+        free(dataArrays->class[j]);
+    }
+    free(dataArrays);
 
     return 0;
 }
